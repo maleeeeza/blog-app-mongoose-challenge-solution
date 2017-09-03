ec7b11abd1d936d67241b1d0e979f54be591a70d
maleeeeza <melissa.k.schoenberg@gmail.com>
2017-09-03 14:53:57 -0500

    test - integration tests created for get, post, put and delete

--- test/blogs-integration.js
+++ test/blogs-integration.js
@@ -5,9 +5,9 @@ const faker = require('faker');
 
 const should = chai.should();
 
-const{BlogPost} = require('../models');
-const{app, runServer, closeServer} = require('../server');
-const{TEST_DATABASE_URL} = require('../config');
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
@@ -16,7 +16,7 @@ function seedBlogPostData() {
   console.info('seeding blog post data');
   const seedData = [];
 
-  for (let i=1; 1<=10; i++){
+  for (let i=1; i<=10; i++){
     seedData.push(generateBlogPostData());
   }
   return BlogPost.insertMany(seedData);
@@ -57,6 +57,148 @@ describe('Blog Posts API resource', function(){
   after(function(){
     return closeServer();
   });
+
+  describe('GET', function(){
+
+    it('should return all existing blog posts', function(){
+      let res;
+      return chai.request(app)
+      .get('/posts')
+      .then(function(_res){
+        res = _res;
+        res.should.have.status(200);
+        res.body.should.have.length.of.at.least(1);
+        return BlogPost.count();
+      })
+      .then(function(count){
+        res.body.should.have.lengthOf(count);
+      });
+
+    });
+
+    it('should return blog posts with right fields', function() {
+
+      let resBlogPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.length.of.at.least(1);
+
+          res.body.forEach(function(post) {
+            post.should.be.a('object');
+            post.should.include.keys(
+              'id', 'title', 'content', 'author', 'created');
+          });
+          resBlogPost = res.body[0];
+          return BlogPost.findById(resBlogPost.id);
+        })
+        .then(function(post) {
+          resBlogPost.id.should.equal(post.id);
+          resBlogPost.title.should.equal(post.title);
+          resBlogPost.content.should.equal(post.content);
+          resBlogPost.author.should.equal(post.author.firstName + ' ' + post.author.lastName);
+          resBlogPost.created.should.contain(post.created.toISOString());
+        });
+    });
+  });
+
+  describe('POST', function() {
+
+    it('should add a new blog post', function() {
+
+      const newBlogPost = {
+        title: 'Skynet: is it really fiction?',
+        content: 'content for skynet story',
+        author: {
+          firstName: 'The',
+          lastName: 'Terminator'}
+        };
+
+      return chai.request(app)
+        .post('/posts')
+        .send(newBlogPost)
+        .then(function(res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys(
+            'id', 'title', 'content', 'author', 'created');
+          res.body.id.should.not.be.null;
+          res.body.title.should.equal(newBlogPost.title);
+          res.body.content.should.equal(newBlogPost.content);
+          res.body.author.should.equal(newBlogPost.author.firstName + ' ' + newBlogPost.author.lastName);
+          res.body.created.should.not.be.null;
+          return BlogPost.findById(res.body.id);
+        })
+        .then(function(post) {
+          post.title.should.equal(newBlogPost.title);
+          post.content.should.equal(newBlogPost.content);
+          post.author.firstName.should.equal(newBlogPost.author.firstName);
+          post.author.lastName.should.equal(newBlogPost.author.lastName);
+          post.created.should.not.be.null;
+        });
+    });
+  });
+
+
+  describe('PUT', function() {
+    it('should update fields', function() {
+      const updateData = {
+        title: 'blahblahblahblah',
+        content: 'more blahblahblahblah'
+      };
+
+      return BlogPost
+        .findOne()
+        .exec()
+        .then(function(post) {
+          updateData.id = post.id;
+          return chai.request(app)
+            .put(`/posts/${post.id}`)
+            .send(updateData);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+
+          return BlogPost.findById(updateData.id).exec();
+        })
+        .then(function(post) {
+          post.title.should.equal(updateData.title);
+          post.content.should.equal(updateData.content);
+        });
+      });
+  });
+
+  describe('DELETE', function() {
+
+    it('delete a blog post by id', function() {
+
+      let post;
+
+      return BlogPost
+        .findOne()
+        .exec()
+        .then(function(_post) {
+          post = _post;
+          return chai.request(app).delete(`/posts/${post.id}`);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+          return BlogPost.findById(post.id).exec();
+        })
+        .then(function(_post) {
+          should.not.exist(_post);
+        });
+    });
+  });
+
+
+
+
+
 
 
 
